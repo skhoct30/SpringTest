7ab1e629a1f08500eea0b73537621a454972423d
skhoct30 <skhoct30@naver.com>
2025-05-28 00:04:34 +0900

    [서강현] - test02 문제풀이

--- src/main/java/com/skhoct30/spring/test/thymeleaf/ThymeleafController.java
+++ src/main/java/com/skhoct30/spring/test/thymeleaf/ThymeleafController.java
@@ -1,14 +1,38 @@
 package com.skhoct30.spring.test.thymeleaf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/thymeleaf")
 public class ThymeleafController {
 	
 	
-	@GetMapping("/thymeleaf/test01")
+	@GetMapping("/test01")
 	public String test01() {
 		return "thymeleaf/test01";
+	}
+	
+	
+	@GetMapping("/test02")
+	public String test02(Model model) {
+		
+		
+		List<String> musicRanking = new ArrayList<>();
+		
+		musicRanking.add("강남스타일");
+		musicRanking.add("벚꽃엔딩");
+		musicRanking.add("좋은날");
+		musicRanking.add("거짓말");
+		musicRanking.add("보고싶다");
+		
+		model.addAttribute("musicRanking", musicRanking);
+		
+		return "thymeleaf/test02";
 	}
 }
