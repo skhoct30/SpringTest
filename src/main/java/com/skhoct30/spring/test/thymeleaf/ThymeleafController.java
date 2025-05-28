6a583f876f60502150aa6989b82f1a775cef7c3f
skhoct30 <skhoct30@naver.com>
2025-05-28 18:08:52 +0900

    [서강현] - Test02 문제 1번 2번 풀이

--- src/main/java/com/skhoct30/spring/test/thymeleaf/ThymeleafController.java
+++ src/main/java/com/skhoct30/spring/test/thymeleaf/ThymeleafController.java
@@ -1,7 +1,9 @@
 package com.skhoct30.spring.test.thymeleaf;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,4 +37,54 @@ public class ThymeleafController {
 		
 		return "thymeleaf/test02";
 	}
+	
+	
+	@GetMapping("/test02.2")
+	public String test03(Model model) {
+		
+		List<Map<String, Object>> membership = new ArrayList<>();
+
+		Map<String, Object> member = new HashMap<>();
+		member.put("name", "손오공");
+		member.put("phoneNumber", "010-1234-5678");
+		member.put("grade", "VIP");
+		member.put("point", 12040);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "저팔계");
+		member.put("phoneNumber", "010-4321-1234");
+		member.put("grade", "BASIC");
+		member.put("point", 3420);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "사오정");
+		member.put("phoneNumber", "010-8888-1111");
+		member.put("grade", "GOLD");
+		member.put("point", 1530);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "삼장");
+		member.put("phoneNumber", "010-0000-1234");
+		member.put("grade", "GOLD");
+		member.put("point", 8450);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "우마왕");
+		member.put("phoneNumber", "010-1111-2222");
+		member.put("grade", "BASIC");
+		member.put("point", 420);
+		membership.add(member);	
+		
+		
+		model.addAttribute("memberList", membership);
+		
+		
+		
+		return "thymeleaf/test02.2";
+	}
+	
 }
